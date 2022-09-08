64676639215c60e1e36b10f0a3e64d376bc3d849
DaveCharlie <rapozocharles@gmail.com>
2022-09-08 15:01:27 -0700

    correction batch

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints given text
+ *
+ * Return:1
+ */
+int main(void)
+{
+  char quo[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+  write(2, quo' 59);
+  return (1);
+}
